8b2575acfca4b21c43c3f03b1d12f8e7b2e5f3e6
Raghavi Srinivasan <53093082+Raghavi94@users.noreply.github.com>
2020-12-05 22:58:26 +0530

    Add files via upload

---
+++ Neo-g-camp/First CLI Quiz App/leap yr exercise.js
@@ -0,0 +1,12 @@
+var readlineSync = require("readline-sync");
+
+const chalk = require('chalk');
+
+var userName = readlineSync.question(chalk.red("What's your name? "));
+var year = readlineSync.question(chalk.blue("Can you tell your year of birth"));
+if((year%4)===0 || (year%100)===0 && (year%400)===0){
+  console.log("Your year of birth " + chalk.green(year) + " is leap year");
+}
+else{
+  console.log("Your year of birth " + chalk.green(year) + " is not a leap year");
+}
