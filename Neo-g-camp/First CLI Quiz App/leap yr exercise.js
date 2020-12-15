6627806de558006e3391e9c9d84787e39be09df7
Raghavi Srinivasan <53093082+Raghavi94@users.noreply.github.com>
2020-12-15 16:35:48 +0530

    Update leap yr exercise.js

--- Neo-g-camp/First CLI Quiz App/leap yr exercise.js
+++ Neo-g-camp/First CLI Quiz App/leap yr exercise.js
@@ -5,8 +5,8 @@ const chalk = require('chalk');
 var userName = readlineSync.question(chalk.red("What's your name? "));
 var year = readlineSync.question(chalk.blue("Can you tell your year of birth"));
 if((year%4)===0 || (year%100)===0 && (year%400)===0){
-  console.log("Your year of birth " + chalk.green(year) + " is leap year");
+  console.log(chalk.magentaBright(userName) + ", your year of birth " + chalk.green(year) + " is leap year");
 }
 else{
-  console.log("Your year of birth " + chalk.green(year) + " is not a leap year");
+  console.log(chalk.magentaBright(userName) + ", your year of birth " + chalk.green(year) + " is not a leap year");
 }
