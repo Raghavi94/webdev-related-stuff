7713f665e1b792386c53a29a7337302f9f9b2826
Raghavi Srinivasan <53093082+Raghavi94@users.noreply.github.com>
2021-01-03 10:50:13 +0530

    Add files via upload

---
+++ LCO JS/01.usersignup.js
@@ -0,0 +1,24 @@
+var firstName = "Raghavi";
+
+var lastName = "Srinivasan";
+
+var email = "raghaveni50@gmail.com";
+
+var password = "12345";
+
+var country = "India";
+
+var isLoggedInFromGoogle = false;
+
+var isLoggedInFromFacebook = false;
+
+console.log(`
+
+First name is : ${firstName}
+Last name is : ${lastName}
+Mail ID is : ${email}
+Password is : ${password}
+Belongs to : ${country}
+
+
+`);
