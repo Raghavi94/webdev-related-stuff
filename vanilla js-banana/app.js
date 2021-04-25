d28509d35d44c70ff871319fa33e80af8b6f47d6
raghavi <raghaveni50@gmail.com>
2021-04-25 23:01:25 +0530

    ex-06:wire fetch call in app

--- vanilla js-banana/app.js
+++ vanilla js-banana/app.js
@@ -2,7 +2,22 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-ip");
 var opText = document.querySelector("#op");
 
-btnTranslate.addEventListener("click",clickEventHandler);
-function clickEventHandler(){
-    opText.innerText = "joojjojo " +txtInput.value;
+var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+function getTranslationURL(text){
+    return serverURL + "?" + "text=" + text
 }
+function clickEventHandler(){
+    //opText.innerText = "lochakmochakpachak " +txtInput.value;
+
+    //taking input
+    var inputText = txtInput.value;
+
+    //calling server for processing
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => console.log(json.contents.translated))
+
+};
+
+btnTranslate.addEventListener("click",clickEventHandler);
