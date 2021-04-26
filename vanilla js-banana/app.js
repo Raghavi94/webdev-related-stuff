b294c193fcf189aacfa5d958444a7903d3b7170c
raghavi <raghaveni50@gmail.com>
2021-04-26 23:23:49 +0530

    replace with mock url

--- vanilla js-banana/app.js
+++ vanilla js-banana/app.js
@@ -2,11 +2,19 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-ip");
 var opText = document.querySelector("#op");
 
-var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+//var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
 
 function getTranslationURL(text){
     return serverURL + "?" + "text=" + text
 }
+
+function errorHandler(error){
+    console.log("error occured",error);
+    alert("something wrong with server! try again later")
+}
+
 function clickEventHandler(){
     //opText.innerText = "lochakmochakpachak " +txtInput.value;
 
@@ -16,7 +24,11 @@ function clickEventHandler(){
     //calling server for processing
     fetch(getTranslationURL(inputText))
         .then(response => response.json())
-        .then(json => console.log(json.contents.translated))
+        .then(json => {
+            var translatedText = json.contents.translated;
+            opText.innerText = translatedText;
+        })
+        .catch(errorHandler)
 
 };
 
